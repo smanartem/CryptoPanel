f92b90f2239831cfc47a8cb81b9261c2bf8c779c
Artsem Barysau <smanartem@gmail.com>
2023-01-24 22:37:57 +0100

    separate app module

--- app/src/main/java/com/example/cryptopanel/App.kt
+++ app/src/main/java/com/example/cryptopanel/App.kt
@@ -1,6 +1,7 @@
 package com.example.cryptopanel
 
 import android.app.Application
+import com.example.cryptopanel.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
--- app/src/main/java/com/example/cryptopanel/di/App_DI.kt
+++ app/src/main/java/com/example/cryptopanel/di/App_DI.kt
@@ -1,18 +1,14 @@
-package com.example.cryptopanel
+package com.example.cryptopanel.di
 
-import com.example.cryptopanel.retrofit.CoinGeckoApi
-import com.example.cryptopanel.retrofit.provideCoinGeckoApi
-import com.example.cryptopanel.retrofit.provideRetrofit
 import com.example.cryptopanel.viewModels.CryptoPanelViewModel
 import com.example.cryptopanel.viewModels.NewsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import retrofit2.Retrofit
 
-val appModule = module {
-    single<CoinGeckoApi> { provideCoinGeckoApi(get()) }
-    single<Retrofit> { provideRetrofit() }
 
+val viewModelModule = module {
     viewModel { CryptoPanelViewModel(get(), get()) }
     viewModel { NewsViewModel(get()) }
 }
+
+val appModule = viewModelModule + networkModule
