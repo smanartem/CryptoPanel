535525559a1916db78a1e7ff0176145c752d2c4a
Artsem-Barysau <artem.borisov@innowise-group.com>
2023-08-20 18:13:28 +0200

    add api request for and create trade coin

---
+++ app/src/main/java/com/example/cryptopanel/data/data/model/TradeCoin.kt
@@ -0,0 +1,7 @@
+package com.example.cryptopanel.data.data.model
+
+data class TradeCoin(
+    val name: String,
+    val price: Double,
+    val priceChange24h: Double
+)
--- app/src/main/java/com/example/cryptopanel/data/data/retrofit/CoinGeckoApi.kt
+++ app/src/main/java/com/example/cryptopanel/data/data/retrofit/CoinGeckoApi.kt
@@ -1,6 +1,7 @@
 package com.example.cryptopanel.data.data.retrofit
 
 import com.example.cryptopanel.data.data.model.CoinDataModel
+import com.example.cryptopanel.data.data.model.TradeCoin
 import com.example.cryptopanel.data.data.model.Trend
 import retrofit2.Response
 import retrofit2.http.GET
@@ -11,4 +12,7 @@ interface CoinGeckoApi {
 
     @GET("http://api.coingecko.com/api/v3/search/trending")
     suspend fun getTrendCoins(): Response<Trend>
+
+    @GET("http://api.coingecko.com/api/v3/simple/price?ids=terra-luna&vs_currencies=usd&include_24hr_change=true")
+    suspend fun getOneCoin(): Response<TradeCoin>
 }
